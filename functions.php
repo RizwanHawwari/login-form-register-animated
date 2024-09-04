fa3c5cb9c65a102b2c7875bac7508a8ccffd92b9
Rizwan Hawwari <rzwnhawari@gmail.com>
2024-09-04 19:05:24 +0700

    Menambahkan function query pada functions.php

--- functions.php
+++ functions.php
@@ -6,4 +6,14 @@ $pass_db    = "";
 $nama_db    = "sekolah";
 $conn    = mysqli_connect($host_db, $user_db, $pass_db, $nama_db);
 
+function query($query) {
+  global $conn;
+  $result = mysqli_query($conn, $query);
+  $rows = [];
+  while ( $row = mysqli_fetch_assoc($result) ) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
+
 ?>

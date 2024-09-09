69f203c57a19327429a70694b51132373f0e0720
Azzam <145013127+azzmsyriff@users.noreply.github.com>
2024-09-10 00:02:04 +0700

    Menambahkan file PHP

--- login.php
+++ login.php
@@ -75,23 +75,21 @@ if (isset($_POST['login'])) {
     }
 }
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
   <link rel="stylesheet" href="css-file/login.css">
-  <!-- <link rel="stylesheet" href="css-file/login.css"> -->
-  <!-- <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
 </head>
 
 <body>
   <div class="container">
+    <div class="img">
+      <img src="img/logo-cn.png" alt="Logo SMK Citra Negara" width="150" height="140">
+    </div>
     <div class="login-box">
       <?php if ($err) { ?>
       <!-- Tampilkan pesan kesalahan jika ada -->
@@ -101,10 +99,8 @@ if (isset($_POST['login'])) {
       <?php } ?>
       <h2>Login</h2>
       <form id="loginform" action="" method="post" role="form" autocomplete="off">
-
         <div class="input-box">
-          <input id="login-username" type="text" name="username" value="<?php echo htmlspecialchars($username) ?>"
-            required>
+          <input id="login-username" type="text" name="username" value="<?php echo htmlspecialchars($username) ?>" required>
           <label>Username</label>
         </div>
 
@@ -113,9 +109,8 @@ if (isset($_POST['login'])) {
           <label>Password</label>
         </div>
 
-        <div class="input-box">
-          <input id="login-remember" type="checkbox" name="ingataku" value="1"
-            <?php if ($ingataku == '1') echo "checked" ?>>
+        <div class="remember-box">
+          <input id="login-remember" type="checkbox" name="ingataku" value="1" <?php if ($ingataku == '1') echo "checked" ?>>
           <label for="login-remember" class="remember">Remember me</label>
         </div>
 
@@ -125,60 +120,8 @@ if (isset($_POST['login'])) {
         <div class="signup-link">
           <a href="register.php">Sign Up</a>
         </div>
+      </form>
     </div>
-    </form>
-
-    <span style="--i:0;"></span>
-    <!-- <span style="--i:1;"></span> -->
-    <span style="--i:2;"></span>
-    <!-- <span style="--i:3;"></span> -->
-    <span style="--i:4;"></span>
-    <!-- <span style="--i:5;"></span> -->
-    <span style="--i:6;"></span>
-    <!-- <span style="--i:7;"></span> -->
-    <span style="--i:8;"></span>
-    <!-- <span style="--i:9;"></span> -->
-    <span style="--i:10;"></span>
-    <!-- <span style="--i:11;"></span> -->
-    <span style="--i:12;"></span>
-    <!-- <span style="--i:13;"></span> -->
-    <span style="--i:14;"></span>
-    <!-- <span style="--i:15;"></span> -->
-    <span style="--i:16;"></span>
-    <!-- <span style="--i:17;"></span> -->
-    <span style="--i:18;"></span>
-    <!-- <span style="--i:19;"></span> -->
-    <span style="--i:20;"></span>
-    <!-- <span style="--i:21;"></span> -->
-    <span style="--i:22;"></span>
-    <!-- <span style="--i:23;"></span> -->
-    <span style="--i:24;"></span>
-    <!-- <span style="--i:25;"></span> -->
-    <span style="--i:26;"></span>
-    <!-- <span style="--i:27;"></span> -->
-    <span style="--i:28;"></span>
-    <!-- <span style="--i:29;"></span> -->
-    <span style="--i:30;"></span>
-    <!-- <span style="--i:31;"></span> -->
-    <span style="--i:32;"></span>
-    <!-- <span style="--i:33;"></span> -->
-    <span style="--i:34;"></span>
-    <!-- <span style="--i:35;"></span> -->
-    <span style="--i:36;"></span>
-    <!-- <span style="--i:37;"></span> -->
-    <span style="--i:38;"></span>
-    <!-- <span style="--i:39;"></span> -->
-    <span style="--i:40;"></span>
-    <!-- <span style="--i:41;"></span> -->
-    <span style="--i:42;"></span>
-    <!-- <span style="--i:43;"></span> -->
-    <span style="--i:44;"></span>
-    <!-- <span style="--i:45;"></span> -->
-    <span style="--i:46;"></span>
-    <!-- <span style="--i:47;"></span> -->
-    <span style="--i:48;"></span>
-    <!-- <span style="--i:49;"></span> -->
   </div>
 </body>
-
 </html>
--- register.php
+++ register.php
@@ -74,102 +74,58 @@ if (isset($_POST['register'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Register</title>
-  <link rel="stylesheet" href="css-file/register.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="css-file/register.css">
 </head>
-
 <body>
-  <div class="container">
-    <div class="login-box">
-      <h2>Create An Account</h2>
-      <?php if ($err) { ?>
-      <div id="register-alert" class="alert">
-        <ul><?php echo $err ?></ul>
-      </div>
-      <?php } ?>
-      <form id="registerform" action="" method="post" role="form" autocomplete="off">
-        <div class="input-box">
-          <input id="register-username" type="text" name="username" value="<?php echo htmlspecialchars($username) ?>"
-            required>
-          <label>Username</label>
-        </div>
-        <div class="input-box">
-          <input id="register-password" type="password" name="password" required>
-          <label>Password</label>
-        </div>
-        <div class="input-box">
-          <input id="register-konfirmasi-password" type="password" name="konfirmasi_password" required>
-          <label>Confirm Password</label>
-        </div>
-        <div class="input-box">
-          <input id="register-email" type="text" name="email" value="<?php echo htmlspecialchars($email) ?>" required>
-          <label>Email</label>
-        </div>
-        <div class="input-box">
-          <input id="register-no-telp" type="text" name="no_telp" value="<?php echo htmlspecialchars($no_telp) ?>"
-            required>
-          <label>Phone Number</label>
-        </div>
-        <input type="submit" name="register" class="btn" value="Register">
-      </form>
-    </div>
 
-    <span style="--i:0;"></span>
-    <!-- <span style="--i:1;"></span> -->
-    <span style="--i:2;"></span>
-    <!-- <span style="--i:3;"></span> -->
-    <span style="--i:4;"></span>
-    <!-- <span style="--i:5;"></span> -->
-    <span style="--i:6;"></span>
-    <!-- <span style="--i:7;"></span> -->
-    <span style="--i:8;"></span>
-    <!-- <span style="--i:9;"></span> -->
-    <span style="--i:10;"></span>
-    <!-- <span style="--i:11;"></span> -->
-    <span style="--i:12;"></span>
-    <!-- <span style="--i:13;"></span> -->
-    <span style="--i:14;"></span>
-    <!-- <span style="--i:15;"></span> -->
-    <span style="--i:16;"></span>
-    <!-- <span style="--i:17;"></span> -->
-    <span style="--i:18;"></span>
-    <!-- <span style="--i:19;"></span> -->
-    <span style="--i:20;"></span>
-    <!-- <span style="--i:21;"></span> -->
-    <span style="--i:22;"></span>
-    <!-- <span style="--i:23;"></span> -->
-    <span style="--i:24;"></span>
-    <!-- <span style="--i:25;"></span> -->
-    <span style="--i:26;"></span>
-    <!-- <span style="--i:27;"></span> -->
-    <span style="--i:28;"></span>
-    <!-- <span style="--i:29;"></span> -->
-    <span style="--i:30;"></span>
-    <!-- <span style="--i:31;"></span> -->
-    <span style="--i:32;"></span>
-    <!-- <span style="--i:33;"></span> -->
-    <span style="--i:34;"></span>
-    <!-- <span style="--i:35;"></span> -->
-    <span style="--i:36;"></span>
-    <!-- <span style="--i:37;"></span> -->
-    <span style="--i:38;"></span>
-    <!-- <span style="--i:39;"></span> -->
-    <span style="--i:40;"></span>
-    <!-- <span style="--i:41;"></span> -->
-    <span style="--i:42;"></span>
-    <!-- <span style="--i:43;"></span> -->
-    <span style="--i:44;"></span>
-    <!-- <span style="--i:45;"></span> -->
-    <span style="--i:46;"></span>
-    <!-- <span style="--i:47;"></span> -->
-    <span style="--i:48;"></span>
-    <!-- <span style="--i:49;"></span> -->
+  <div class="img">
+    <img src="img/logo-cn.png" alt="Logo SMK Citra Negara" width="150px" height="150px">
   </div>
-</body>
 
+    <div class="container">
+        <div class="login-box">
+            <h2>Create An Account</h2>
+            <form id="registerform" action="" method="post" role="form" autocomplete="off">
+                
+                <div class="row">
+                    <div class="input-box">
+                        <input id="register-username" type="text" name="username" value="<?php echo htmlspecialchars($username) ?>" required>
+                        <label>Username</label>
+                    </div>
+                    <div class="input-box">
+                        <input id="register-email" type="text" name="email" value="<?php echo htmlspecialchars($email) ?>" required>
+                        <label>Email</label>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="input-box">
+                        <input id="register-password" type="password" name="password" required>
+                        <label>Password</label>
+                    </div>
+                    <div class="input-box">
+                        <input id="register-konfirmasi-password" type="password" name="konfirmasi_password" required>
+                        <label>Confirm Password</label>
+                    </div>
+                </div>
+
+                <div class="input-box full-width">
+                    <input id="register-no-telp" type="text" name="no_telp" value="<?php echo htmlspecialchars($no_telp) ?>" required>
+                    <label>Phone Number</label>
+                </div>
+
+                <input type="submit" name="register" class="btn" value="Register">
+                
+                <div class="login-btn">
+                    <a href="login.php">Log In</a>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
 </html>

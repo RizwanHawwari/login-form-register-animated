28b7f74a4931a0f38e40ccd8f97cfab90677e17a
Rizwan Hawwari <rzwnhawari@gmail.com>
2024-09-15 20:39:26 +0700

    Memperbarui tampilan anggota.php

--- anggota.php
+++ anggota.php
@@ -1,14 +1,52 @@
+<?php 
+session_start();
+require "functions.php";
+
+if ( !isset( $_SESSION["session_username"] ) ) {
+  header("Location: login.php");
+  exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="css-file/main.css">
+  <title>Rookies School Management</title>
 </head>
 
 <body>
-  <h2>HI</h2>
+  <div class="container">
+    <div class="header">
+      <h2>Rookies School Management</h2>
+    </div>
+    <div class="kategori">
+      <div class="siswa">
+        <figure>
+          <img src="img/siswa.png" alt="siswa" width="250">
+          <!-- <figcaption>Siswa</figcaption> -->
+        </figure>
+        <button><a href="siswa.php">Data Siswa</a></button>
+      </div>
+      <div class="guru">
+        <figure>
+          <img src="img/guru.png" alt="guru" width="250">
+          <!-- <figcaption>Guru</figcaption> -->
+        </figure>
+        <button><a href="guru.php">Data Guru</a></button>
+      </div>
+      <div class="mapel">
+        <figure>
+          <img src="img/mapel.png" alt="mapel" width="250">
+          <!-- <figcaption>Mapel</figcaption> -->
+        </figure>
+        <button><a href="mapel.php">Data Mapel</a></button>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>

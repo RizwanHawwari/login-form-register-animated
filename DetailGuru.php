96932969ab5b0c1412d786986f5342cadeeaf292
Rizwan Hawwari <rzwnhawari@gmail.com>
2024-09-17 22:04:37 +0700

    Menambahkan semua file nya kembali

---
+++ DetailGuru.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+require "functions.php";
+$id = $_GET["id"];
+
+$guru = query("SELECT * FROM guru WHERE id = $id")[0];
+
+if (!isset($_SESSION['session_username'])) {
+    $username = "Guest"; // Menampilkan pesan default jika tidak ada username di sesi
+} else {
+    $username = htmlspecialchars($_SESSION['session_username']);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
+    session_unset();
+    session_destroy();
+    setcookie("cookie_username", "", time() - 3600, "/");
+    setcookie("cookie_password", "", time() - 3600, "/");
+    header("Location: login.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Guru</title>
+  <link rel="stylesheet" href="css-file/anggota.css">
+</head>
+
+<body>
+  <div class="user-info">
+    <p><strong>Username:</strong> <?php echo $username; ?></p>
+  </div>
+
+  <div class="container">
+    <h1>Detail Data Guru</h1>
+    <table border="1" cellpadding="10" cellspacing="0">
+      <tr>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>No Telp</th>
+        <th>Guru Mapel</th>
+        <th>Jenis Kelamin</th>
+      </tr>
+      <tr>
+        <td><?= $guru["nama"]; ?></td>
+        <td><?= $guru["email"]; ?></td>
+        <td><?= $guru["no_telp"]; ?></td>
+        <td><?= $guru["guru_mapel"]; ?></td>
+        <td><?= $guru["jenis_kelamin"]; ?></td>
+      </tr>
+    </table>
+
+    <a href="guru.php">Kembali ke daftar guru</a>
+
+    <div class="logout-link">
+      <form action="<?= $_SERVER['PHP_SELF']; ?>" method="post">
+        <input type="submit" name="logout" class="btn" value="Logout">
+      </form>
+    </div>
+  </div>
+</body>
+
+</html>
---
+++ sekolah.sql
@@ -0,0 +1,109 @@
+-- MariaDB dump 10.19  Distrib 10.4.28-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: sekolah
+-- ------------------------------------------------------
+-- Server version	10.4.28-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `guru`
+--
+
+DROP TABLE IF EXISTS `guru`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `guru` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `no_telp` varchar(20) NOT NULL,
+  `guru_mapel` int(100) NOT NULL,
+  `jenis_kelamin` enum('Laki-laki','Perempuan') DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_guru_mapel` (`guru_mapel`),
+  CONSTRAINT `fk_guru_mapel` FOREIGN KEY (`guru_mapel`) REFERENCES `mapel` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `guru`
+--
+
+LOCK TABLES `guru` WRITE;
+/*!40000 ALTER TABLE `guru` DISABLE KEYS */;
+INSERT INTO `guru` VALUES (1,'slamet','slamet@gmail.com','08125678123',1,'Laki-laki');
+/*!40000 ALTER TABLE `guru` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mapel`
+--
+
+DROP TABLE IF EXISTS `mapel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mapel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kode` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mapel`
+--
+
+LOCK TABLES `mapel` WRITE;
+/*!40000 ALTER TABLE `mapel` DISABLE KEYS */;
+INSERT INTO `mapel` VALUES (1,'K03','PAI'),(3,'K08','PJOK'),(4,'K55','SEJARAH');
+/*!40000 ALTER TABLE `mapel` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `siswa`
+--
+
+DROP TABLE IF EXISTS `siswa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `siswa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) NOT NULL,
+  `nis` int(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `no_telp` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `siswa`
+--
+
+LOCK TABLES `siswa` WRITE;
+/*!40000 ALTER TABLE `siswa` DISABLE KEYS */;
+INSERT INTO `siswa` VALUES (1,'Rizwan Ajahhs',123123,'rzwnajah@gmail.com','+62895123123'),(17,'cartiers',3242341,'ssawisja@yahoo.com','089566112');
+/*!40000 ALTER TABLE `siswa` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-17 21:40:53
